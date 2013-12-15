25bd56d05ee02ab8fd04b524e95897cdbafa705a
Yang Wang <yang.wang.dec@gmail.com>
2013-12-14 20:10:33 -0500

    cover

--- app.py
+++ app.py
@@ -29,59 +29,59 @@ app.debug = True
 
 
 
-# upload file section
-ALLOWED_EXTENSIONS = set(['png','jpg','jpeg','git'])
+# # upload file section
+# ALLOWED_EXTENSIONS = set(['png','jpg','jpeg','git'])
 
-def allowed_file(filename):
-	return '.' in filename and \
-		filename.lower().rsplit('.',1)[1] in ALLOWED_EXTENSIONS
-@app.route("/uploadfile",methods=['GET','POST'])
-def upload():
-	if request.method == "POST":
-		uploaded_file = request.files['fileupload']
+# def allowed_file(filename):
+# 	return '.' in filename and \
+# 		filename.lower().rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+# @app.route("/uploadfile",methods=['GET','POST'])
+# def upload():
+# 	if request.method == "POST":
+# 		uploaded_file = request.files['fileupload']
 
-		if uploaded_file and allowed_file(uploaded_file.filename):
-			now = datatime.datatime.now()
-			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
+# 		if uploaded_file and allowed_file(uploaded_file.filename):
+# 			now = datatime.datatime.now()
+# 			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
 
-			s3conn = boto.connect_s3(os.environ.get('AKIAJQUKNHDWRE54E7YQ'),os.environ.get('koqewygr3wZh0hZ+Aw1sLXoTobIDyUDCtqQ6zKkV'))
+# 			s3conn = boto.connect_s3(os.environ.get('AKIAJQUKNHDWRE54E7YQ'),os.environ.get('koqewygr3wZh0hZ+Aw1sLXoTobIDyUDCtqQ6zKkV'))
 
-			# open s3 bucket, create new Key/file
-			# set the mimetype, content and access control
-			b = s3conn.get_bucket(os.environ.get('thebalance')) # bucket name defined in .env
-			k = b.new_key(b) # create a new Key (like a file)
-			k.key = filename # set filename
-			k.set_metadata("Content-Type", uploaded_file.mimetype) # identify MIME type
-			k.set_contents_from_string(uploaded_file.stream.read()) # file contents to be added
-			k.set_acl('public-read') # make publicly readable
+# 			# open s3 bucket, create new Key/file
+# 			# set the mimetype, content and access control
+# 			b = s3conn.get_bucket(os.environ.get('thebalance')) # bucket name defined in .env
+# 			k = b.new_key(b) # create a new Key (like a file)
+# 			k.key = filename # set filename
+# 			k.set_metadata("Content-Type", uploaded_file.mimetype) # identify MIME type
+# 			k.set_contents_from_string(uploaded_file.stream.read()) # file contents to be added
+# 			k.set_acl('public-read') # make publicly readable
  
-			# if content was actually saved to S3 - save info to Database
-			if k and k.size > 0:
+# 			# if content was actually saved to S3 - save info to Database
+# 			if k and k.size > 0:
 				
-				submitted_image = models.Image()
-				submitted_image.description = request.form.get('description')
-				submitted_image.filename = filename # same filename of s3 bucket file
-				submitted_image.save()
+# 				submitted_image = models.Image()
+# 				submitted_image.description = request.form.get('description')
+# 				submitted_image.filename = filename # same filename of s3 bucket file
+# 				submitted_image.save()
  
  
-			return redirect('/')
+# 			return redirect('/')
  
-		else:
-			return "uhoh there was an error " + uploaded_file.filename
+# 		else:
+# 			return "uhoh there was an error " + uploaded_file.filename
 
  
  
-	else:
-		# get existing images
-		images = models.Image.objects.order_by('-timestamp')
+# 	else:
+# 		# get existing images
+# 		images = models.Image.objects.order_by('-timestamp')
 		
-		# render the template
-		templateData = {
-			'images' : images,
-			'form' : photo_upload_form
-		}
+# 		# render the template
+# 		templateData = {
+# 			'images' : images,
+# 			'form' : photo_upload_form
+# 		}
  
-		return render_template("main.html", **templateData)
+# 		return render_template("main.html", **templateData)
 
 
 
